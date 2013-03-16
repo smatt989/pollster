58bb3fd7328ec995f56785d2a0dbe64dd43d5386
Matt Slotkin <matthew.slotkin@gmail.com>
2013-03-16 15:38:52 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 PollsterApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  match "/auth/:provider/callback" => "sessions#create"
-  match "/signout" => "sessions#destroy", :as => :signout
-  root :to => 'static_pages#home'
+  root to: 'static_pages#home'
+
+  match '/home', to: 'static_pages#home'
+  match '/about', to: 'static_pages#about'
+  match '/help', to: 'static_pages#help'
+  
 end
