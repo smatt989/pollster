8af94ce562a001fc49ba3c4a909043a864f34e00
Matt Slotkin <matthew.slotkin@gmail.com>
2013-03-16 15:00:16 -0400

    set root

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ PollsterApp::Application.routes.draw do
   get "static_pages/about"
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
+  root :to => 'static_pages#home'
 end
