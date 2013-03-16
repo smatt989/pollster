38c0eb024642c6a7f77fefeab5438affddcafeb9
Matt Slotkin <matthew.slotkin@gmail.com>
2013-03-16 15:43:28 -0400

    prettified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 PollsterApp::Application.routes.draw do
   root to: 'static_pages#home'
-
-  match '/home', to: 'static_pages#home'
+  match "/auth/:provider/callback" => "sessions#create"
+  match "/signout" => "sessions#destroy", :as => :signout
   match '/about', to: 'static_pages#about'
   match '/help', to: 'static_pages#help'
-  
 end
