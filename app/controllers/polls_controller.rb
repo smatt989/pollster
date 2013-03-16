416e83c1b47820e2df46ad935b76811f971153d7
Matt Slotkin <matthew.slotkin@gmail.com>
2013-03-16 16:42:43 -0400

    added poll creation i think

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,4 +1,16 @@
 class PollsController < ApplicationController
   def new
   end
+
+  def create
+    @poll = current_user.polls.build(params[:poll])
+    if @poll.save
+      flash[:success] = "poll created!"
+      redirect_to root_url
+    else
+      flash[:error] = "failed to create poll"
+      render 'static_pages/home'
+    end
+  end
+
 end
