172e0121ddbb5923123d0c26277ab60d6adaae12
Matt Slotkin <matthew.slotkin@gmail.com>
2013-03-16 17:06:40 -0400

    added user_id to polls

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130316200940) do
+ActiveRecord::Schema.define(:version => 20130316210618) do
 
   create_table "polls", :force => true do |t|
     t.string   "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
