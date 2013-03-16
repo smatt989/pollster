02566853d0aac143192deab6edc1974afb1a7600
Matt Slotkin <matthew.slotkin@gmail.com>
2013-03-16 14:30:42 -0400

    small bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-  	auth = request.evn["omniauth.auth"]
+  	auth = request.env["omniauth.auth"]
   	user = User.find_by_provider_and_uid(auth["provider", auth]["uid"]) || User.create_with_omniauth(auth)
   	session[:user_id] = user.user_id
   	redirect_to root_url, :notice => "Signed in!"
