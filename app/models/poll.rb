e99ee2a595a92864e58fba8e1cf9a4ce846aba33
Matt Slotkin <matthew.slotkin@gmail.com>
2013-03-17 00:45:56 -0400

    allows for blank answer 3 and 4

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -6,8 +6,8 @@ class Poll < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
   validates :answer_1, presence: true, length: { minimum: 1, maximum: 50 }
   validates :answer_2, presence: true, length: { minimum: 1, maximum: 50 }
-  validates :answer_3, length: { minimum: 1, maximum: 50 }
-  validates :answer_4, length: { minimum: 1, maximum: 50 }
+  validates :answer_3, length: { minimum: 1, maximum: 50 }, :allow_blank => true
+  validates :answer_4, length: { minimum: 1, maximum: 50 }, :allow_blank => true
 
   default_scope order: 'polls.created_at DESC'
 end
