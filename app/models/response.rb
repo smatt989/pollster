88a0fbed8fafbff0cefd3f5d7f451b87d3896eac
Matt Slotkin <matthew.slotkin@gmail.com>
2013-03-17 22:50:24 -0400

    cleaned up response model

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,4 +2,7 @@ class Response < ActiveRecord::Base
   attr_accessible :content, :poll_id, :response_type, :user_id
   belongs_to :user
   belongs_to :poll
+  validates :content, presence: true, length: { minimum: 1, maximum: 50 }
+  validates :poll_id, presence: true
+  validates :response_type, presence: true
 end
