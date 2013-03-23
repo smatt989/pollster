d51c41278ffcc5a5e8380cf6f29fb3c44056e8cf
Matt Slotkin <matthew.slotkin@gmail.com>
2013-03-23 15:59:30 -0400

    removed protect_from_forgery

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,10 @@ class SessionsController < ApplicationController
   	@user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
   	#session[:user_id] = @user.uid
     sign_in @user
-  	#redirect_to root_url, :notice => "Signed in!"
-    render json: @user.as_json(:only => [:name])
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { render json: @user.as_json(:only => [:name]) }
+    end
   end
 
   def destroy
