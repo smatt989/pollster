42453e1be908408baee2fa4b35befe4d61386d1c
Matt Slotkin <matthew.slotkin@gmail.com>
2013-03-23 16:02:26 -0400

    fixed json on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,7 @@ class SessionsController < ApplicationController
   	@user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
   	#session[:user_id] = @user.uid
     sign_in @user
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.json { render json: @user.as_json(:only => [:name]) }
-    end
+    render json: @user.as_json(:only => [:name])
   end
 
   def destroy
